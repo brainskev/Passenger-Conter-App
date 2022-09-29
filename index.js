ef84676e90bfea5bf291930844ba07c5cc4e2a60
Kelvin <ngotho.kelvin@yahoo.com>
2022-09-29 19:40:29 +0300

    created function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+//initialize the count as 0
+//listen for clicks on the increament button
+//icreament variable when button is clicked
+//change the count-el in the HTML to reflect the new count
+
+let countEl = document.getEmentById("count-el")
+
+let count = o
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count                                            
+
+}
+funtion SVGAElement() {
+    console.log
+
+}
