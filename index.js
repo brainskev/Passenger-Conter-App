f20151db5a7d51d3acc35474580c64d8a45b413c
Kelvin <ngotho.kelvin@yahoo.com>
2022-09-29 21:17:26 +0300

    added save function

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 let count = 0
-//let saveEl = document.getElementById("save-el")
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 
 function increment() {
     count += 1
     countEl.textContent = count
 }
+
+function save() {
+    let countStr = count + " - "
+    saveEl.innerText = countStr
+   
+}
+
+console.log(count)
