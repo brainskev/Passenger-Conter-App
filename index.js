2d9abfb4f49851c99ffe4613b8166811bd56a981
Kelvin <ngotho.kelvin@yahoo.com>
2022-09-29 19:51:39 +0300

    styles.css

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-
-//initialize the count as 0
-//listen for clicks on the increament button
-//icreament variable when button is clicked
-//change the count-el in the HTML to reflect the new count
-
-let countEl = document.getEmentById("count-el")
-
-let count = o
+let count = 0
+//let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 
 function increment() {
     count = count + 1
-    countEl.innerText = count                                            
-
-}
-funtion SVGAElement() {
-    console.log
-
+    countEl.textContent = count
 }
